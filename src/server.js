00b6549105de80d6fe1d80895ed200e19594f1f4
Thai Quang <thaiquangqt2003@gmail.com>
2024-12-05 16:03:56 +0700

    update

--- src/server.js
+++ src/server.js
@@ -12,11 +12,40 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["https://ung-dung-giay-dep-fontend.vercel.app"],
+    origin: "https://ung-dung-giay-dep-fontend.vercel.app",
     methods: ["POST", "PUT", "PATCH", "GET", "DELETE"],
     credentials: true,
   })
 );
+
+app.options("*", (req, res) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://ung-dung-giay-dep-fontend.vercel.app"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.sendStatus(204); // No Content
+});
+
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 
 app.use(cookieParser());
 
